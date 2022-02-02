e14a1e07387378fd0f7973ace6b332a60a7ac42c
bhagyashri hubale <bhagyashrihubale2@gmail.com>
2022-02-02 22:28:29 +0530

    first comit

---
+++ web/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+      <Navbar/>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/menu"  component={Menu}/>
+          <Route path="/about"  component={About}/>
+          <Route path="/contact"  component={Contact}/>
+          
+        </Switch>
+        <Footer />
+      </Router>
+ 
+    </div>
+  );
+}
+
+export default App;
---
+++ web/src/pages/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function about() {
+  return <div>
+      <h2>About</h2>
+  </div>;
+}
+
+export default about;
